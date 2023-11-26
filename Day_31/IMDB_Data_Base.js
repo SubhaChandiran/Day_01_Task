0b0c3f39bfc621b69825e9be8e455df3e1dd5f2f
SubhaChandiran <jerrysubash@gmail.com>
2023-11-26 21:48:20 +0530

    day31_updated

--- Day_31/IMDB_Data_Base.js
+++ Day_31/IMDB_Data_Base.js
@@ -8,13 +8,13 @@
     5. Artist can perform multiple role in a single film
 
     List of tables:
-        - Movie Collection
-        - Genre Collection
-        - Review Collection
-        - User Collection
-        - Artist Collection
-        - Skill Collection
-        - Role Collection
+        - Movie
+        - Genre 
+        - Review 
+        - User 
+        - Artist 
+        - Skill 
+        - Role 
 
     Relationships:
         - Movie and Review: One-to-Many
@@ -25,44 +25,48 @@
 
     List of tables and columns:
 
-        1. Movie Collection:
-            - _id (Primary key)
+        1. Movie:
+            - movie_id (Primary key)
             - title
             - release_date
             - media (Array of Video/Image objects)
             - genres (Array of genres IDs)
             - reviews (Array of review IDs)
+            - artist_id (foreign key referencing Artist)
         
-        2. Genre Collection:
-            - _id (Primary key)
+        2. Genre:
+            - genre_id (Primary key)
             - name
 
-        3. Review Collection:
-            - _id (Primary key)
+        3. Review :
+            - review_id (Primary key)
             - movie_id (foreign key referencing Movie)
-            - user_id (foreign key referencing user Collection)
+            - user_id (foreign key referencing user)
             - rating
             - comment
             - timestamp
 
-        4. User Collection:
-            - _id (Primary key)
+        4. User:
+            - user_id (Primary key)
             - username
             - email
             - password
 
-        5. Artist Collection:
-            - _id (Primary key)
+        5. Artist:
+            - artist_id (Primary key)
             - name
             - skills (Array of skill IDs)
             - roles (Array of Role IDs)
+            - movie_id (foreign key referencing Artist)
 
-        6. Skill Collection:
-            - _id (Primary key)
+        6. Skill:
+            - skill_id (Primary key)
             - name
+            - artist_id (foreign key referencing Artist)
         
-        7. Role Collection
-            - _id (Primary key)
+        7. Role
+            - Role_id (Primary key)
             - name
+            - artist_id (foreign key referencing Artist)
 
 */
